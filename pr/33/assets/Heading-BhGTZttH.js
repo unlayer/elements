6f2023f49c1aaabe0c4fabcd64d1dc36337bfc18
ivoIturrieta <ivoIturrieta@users.noreply.github.com>
2026-06-28 14:08:49 +0000

    deploy: 973a4f83bfe30bc338c40cfe82ce9eebd6db5042

---
+++ pr/33/assets/Heading-BhGTZttH.js
@@ -0,0 +1,1 @@
+import{c as o,r as s,m as p,t as i}from"./create-component-CyQuuYXd.js";const t={...s,color:"#000000",fontWeight:400,text:"Heading"},d=o({name:"Heading",defaultValues:t,propMapper:n=>{const{level:a,...r}=n,e=p(r,t,"Heading");return a&&!e.headingType&&(e.headingType=a),e},displayName:"Heading",exporters:i});export{d as H};
