4b6ce0f07b2d15792127d4c3e35e774ecdc1fba3
ivoIturrieta <ivoIturrieta@users.noreply.github.com>
2026-06-28 13:42:29 +0000

    deploy: c9e729155bfe433fe87d245f5ffc3f7fcfb681dc

---
+++ pr/33/assets/Paragraph-DEyEtYzv.js
@@ -0,0 +1,1 @@
+import{c as p,m as e,z as o,A as s}from"./create-component-D2PtwBvG.js";const a={...o,color:"#000000"},m=p({name:"Paragraph",defaultValues:a,propMapper:r=>e(r,a,"Paragraph"),displayName:"Paragraph",exporters:s});export{m as P};
