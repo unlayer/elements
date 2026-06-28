077cb3309ba96f87e733c8ef0ad148c915dd00c9
ivoIturrieta <ivoIturrieta@users.noreply.github.com>
2026-06-28 15:42:02 +0000

    deploy: 47c61d90ff0ff7eff5f4f1a1dc8c95e18608d6ba

--- assets/Paragraph-D7ylb_vL.js
+++
@@ -1,1 +0,0 @@
-import{c as p,m as e,z as o,A as s}from"./create-component-BRXiI3nx.js";const a={...o,color:"#000000"},m=p({name:"Paragraph",defaultValues:a,propMapper:r=>e(r,a,"Paragraph"),displayName:"Paragraph",exporters:s});export{m as P};
