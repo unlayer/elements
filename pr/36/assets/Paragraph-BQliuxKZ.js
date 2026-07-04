886a68aeb954ec1562e6ddb51bd6a81801ff73e5
ivoIturrieta <ivoIturrieta@users.noreply.github.com>
2026-07-04 07:58:21 +0000

    deploy: b8e45432ebd2f935f12248f90e7e3edac27c8667

---
+++ pr/36/assets/Paragraph-BQliuxKZ.js
@@ -0,0 +1,1 @@
+import{c as p,m as e,A as o,B as s}from"./create-component-CXhWhFJO.js";const a={...o,color:"#000000"},m=p({name:"Paragraph",defaultValues:a,propMapper:r=>e(r,a,"Paragraph"),displayName:"Paragraph",exporters:s});export{m as P};
--- pr/36/assets/Paragraph-D6GT5WJo.js
+++
@@ -1,1 +0,0 @@
-import{c as p,m as e,A as o,B as s}from"./create-component-T95yoBiY.js";const a={...o,color:"#000000"},m=p({name:"Paragraph",defaultValues:a,propMapper:r=>e(r,a,"Paragraph"),displayName:"Paragraph",exporters:s});export{m as P};
